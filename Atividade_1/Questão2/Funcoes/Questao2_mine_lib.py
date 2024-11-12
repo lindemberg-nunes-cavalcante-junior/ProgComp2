7654a0149e834c0e0315209dd00ab76b58dcdd26
Lindemberg <lindembergjnr@gmail.com>
2024-11-12 14:05:55 -0300

    Fazendo a questão 2

--- Atividade_1/Questão2/Funcoes/Questao2_mine_lib.py
+++ Atividade_1/Questão2/Funcoes/Questao2_mine_lib.py
@@ -1,9 +1,14 @@
 import hashlib
 
 def FindNonce(strToHash:str,intBitsZero:int):
+    BitsZero = 0
     for i in range(2**32):
         a = i.to_bytes(4,'big')
-        x = a + strToHash.encode()
-        HEX = hashlib.sha256(x).digest()
-        print(HEX)
+        x = a + strToHash.encode(encoding='UTF-8', errors='strict')
+        hex = hashlib.sha256(x).digest()
+        teste = bin(hex[1])
+        teste2 = hex[1]
+        teste3 = teste[2:]
+        print(teste,teste2,teste3)
+
     return 'Olá',10
